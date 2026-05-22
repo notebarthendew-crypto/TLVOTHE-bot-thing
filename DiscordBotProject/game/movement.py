8c229aa014d686858db9469014b12910939ee7ec
cardenascamilo-dot <cardenas.camilo@maximino.edu.co>
2026-05-22 11:51:57 -0500

    Update movement.py

--- DiscordBotProject/game/movement.py
+++ DiscordBotProject/game/movement.py
@@ -1,1 +1,44 @@
 # the main code for moving around the map
+
+from discord import app_commands
+import discord
+
+from game.player import players, create_player
+from game.movement import move_player
+
+GAME_ROLE_ID = 123456789012345678 #i dont have my pc right now to get the role i gotta change this
+
+@bot.tree.command(name="move", description="Move through the train")
+@app_commands.describe(direction="front or back")
+async def move(interaction: discord.Interaction, direction: str):
+
+    member = interaction.user
+
+    # Check role
+    has_role = any(role.id == GAME_ROLE_ID for role in member.roles)
+
+    if not has_role:
+        await interaction.response.send_message(
+            "You ain't playing.",
+            ephemeral=True
+        )
+        return
+
+    user_id = str(member.id)
+
+    if user_id not in players:
+        create_player(user_id)
+
+    result = move_player(players[user_id], direction)
+
+    if result is None:
+        await interaction.response.send_message(
+            "You can't go that way.",
+            ephemeral=True
+        )
+        return
+
+    await interaction.response.send_message(
+        f"You moved to {result}.",
+        ephemeral=True
+    )
