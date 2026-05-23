db5cd2626493d1a55c3464919123a9e13c4db6b5
notebarthendew-crypto <notebarthendew@gmail.com>
2026-05-22 20:57:09 -0500

    Update commands.py

--- DiscordBotProject/cogs/commands.py
+++ DiscordBotProject/cogs/commands.py
@@ -2,7 +2,7 @@ from discord import app_commands
 import discord
 
 def setup_commands(bot):
-  
-   @bot.tree.command(name="ping")
-    async def ping(interaction: discord.Interaction):
-        await interaction.response.send_message("Pong!")
+
+  @bot.tree.command(name="ping")
+  async def ping(interaction: discord.Interaction):
+    await interaction.response.send_message("Pong!")
