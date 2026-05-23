84f5890e5f624c9ccb6a32ba64fe9473109330fd
notebarthendew-crypto <notebarthendew@gmail.com>
2026-05-22 21:19:05 -0500

    Update prefix_commands.py

--- DiscordBotProject/cogs/prefix_commands.py
+++ DiscordBotProject/cogs/prefix_commands.py
@@ -2,7 +2,7 @@ from discord import app_commands
 import discord
 
 def setup_commands(bot):
-
+    
     @bot.command()
     async def hello(ctx):
         await ctx.send(f"hi {ctx.author.mention}")
@@ -14,3 +14,8 @@ def setup_commands(bot):
     @bot.command()
     async def cheese(ctx):
         await ctx.send("https://tenor.com/view/crowpunk-cheese-challenge-repost-to-cheese-cat-cheese-cat-gif-4123783956472567463")
+
+    @bot.command()
+    async def revolver(ctx):
+        await ctx.reply(f"Heres a gun"
+                       "# <:Revolver:1505709394057494659>")
