18084e67bd9c68fbbaccdc747a9541fb5ed7a796
TheSnowyDodoAlt <cardenas.camilo@maximino.edu.co>
2026-05-28 08:24:04 -0500

    Update movement.py

--- DiscordBotProject/game/movement.py
+++ DiscordBotProject/game/movement.py
@@ -1,1 +1,16 @@
 # the main code for moving around the map
+
+from game.map import ROOMS
+
+def move_player(player_data, direction):
+
+    current_room = player_data["room"]
+
+    if direction not in ROOMS[current_room]:
+        return None
+
+    new_room = ROOMS[current_room][direction]
+
+    player_data["room"] = new_room
+
+    return new_room
