b1d6f8946fcf8f7be73105b3a3d608851661c641
TheSnowyDodo <notebarthendew@gmail.com>
2026-05-29 18:24:49 -0500

    Update commands.py

--- DiscordBotProject/cogs/commands.py
+++ DiscordBotProject/cogs/commands.py
@@ -7,15 +7,15 @@ def setup_commands(bot):
   async def ping(interaction: discord.Interaction):
     await interaction.response.send_message("Pong!")
 
-   @bot.tree.command(name="commands")
-    async def commands(interaction: discord.Interaction):
-        await interaction.response.send_message(" # TVLOTHE BOT COMMANDS\n"
-                                                "## Slash Commands\n"
-                                                "* '/ping' Send pong.\n"
-                                                "## Prefix Commands\n"
-                                                "* '?hello' Say hi and ping the caller\n"
-                                                "* '?reply' Send a discord reply to the caller\n"
-                                                "* '?cheese' Send a cheese gif (not random)\n"
-                                                "* '?revolver' Send a Revolver (<:Revolver:1505709394057494659>) emoji"
-                                                )
+  @bot.tree.command(name="commands")
+  async def commands(interaction: discord.Interaction):
+    await interaction.response.send_message(" # TVLOTHE BOT COMMANDS\n"
+                                            "## Slash Commands\n"
+                                            "* '/ping' Send pong.\n"
+                                            "## Prefix Commands\n"
+                                            "* '?hello' Say hi and ping the caller\n"
+                                            "* '?reply' Send a discord reply to the caller\n"
+                                            "* '?cheese' Send a cheese gif (not random)\n"
+                                            "* '?revolver' Send a Revolver (<:Revolver:1505709394057494659>) emoji"
+                                            )
         print("Sent 'commands' slash command")
